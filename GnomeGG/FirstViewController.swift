fcab2dee29346d4686143e8052d85ca86045c701
voloshink <voloshin.k@husky.neu.edu>
2019-05-30 22:18:32 -0400

    Initial Commit

---
+++ GnomeGG/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  GnomeGG
+//
+//  Created by Kirill Voloshin on 5/30/19.
+//  Copyright © 2019 Kirill Voloshin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GnomeGG/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GnomeGG
+//
+//  Created by Kirill Voloshin on 5/30/19.
+//  Copyright © 2019 Kirill Voloshin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
